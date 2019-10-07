dd07f9aa6d45debd15e7b02095481c13775123a2
Spencer Miskoviak <smiskoviak@gmail.com>
2019-10-06 19:12:41 -0700

    Add a type checking command and declare test packages

---
+++ declarations.d.ts
@@ -0,0 +1,12 @@
+// These packages are not actually used but are used in the test input/output
+// to verify the transform functionality. They're declared here so type-checking
+// can still be performed on all files.
+declare module "react";
+declare module "@fortawesome/free-brands-svg-icons";
+declare module "@fortawesome/free-solid-svg-icons";
+declare module "@fortawesome/free-regular-svg-icons";
+declare module "@fortawesome/pro-regular-svg-icons";
+declare module "@fortawesome/pro-solid-svg-icons";
+declare module "@fortawesome/pro-light-svg-icons";
+declare module "@fortawesome/react-fontawesome";
+declare module "custom-design-system";
